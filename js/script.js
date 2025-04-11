0ec1eb7febe8adfdb716687ff965427d2d3e9c54
Marzena Tomaszewska <marzenatomaszewska20@gmail.com>
2025-04-11 08:40:42 +0200

    Remove active class

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 'use strict';
 
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event)
-    console.log(event.detail)
-    console.log(event.target
-        
-    )
-  
-    /* remove class 'active' from all article links  */
+ 
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
+
+
   
     /* add class 'active' to the clicked link */
   
-    /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('article.active');
+
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   
